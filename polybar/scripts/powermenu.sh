34df72559c639c99f0e902eb92fc4b9799f32096
RimuRimuh <68438095+RimuhRimu@users.noreply.github.com>
2022-04-04 20:12:55 -0400

    added polybar

---
+++ polybar/scripts/powermenu.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+dir="~/.config/polybar/scripts/rofi"
+uptime=$(uptime -p | sed -e 's/up //g')
+
+rofi_command="rofi -theme $dir/powermenu.rasi"
+
+# Options
+shutdown=" Shutdown"
+reboot=" Restart"
+lock=" Lock"
+suspend=" Sleep"
+logout=" Logout"
+
+# Confirmation
+confirm_exit() {
+	rofi -dmenu\
+		-i\
+		-no-fixed-num-lines\
+		-p "Are You Sure? : "\
+		-theme $dir/confirm.rasi
+}
+
+# Message
+msg() {
+	rofi -theme "$dir/message.rasi" -e "Available Options  -  yes / y / no / n"
+}
+
+# Variable passed to rofi
+options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
+
+chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
+case $chosen in
+    $shutdown)
+      systemctl poweroff
+    ;;
+    $reboot)
+			systemctl reboot
+    ;;
+    $lock)
+      dm-tool lock
+    ;;
+    $suspend)
+      mpc -q pause
+      amixer set Master mute
+      systemctl suspend
+    ;;
+    $logout)
+      xfce4-session-logout
+    ;;
+esac
